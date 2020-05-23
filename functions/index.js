fdc88a6c581ed6b99ff0e1493f4ca88042bdfef7
Alanis Yates <yatesalanis@gmail.com>
2020-05-23 12:10:04 -0400

    update todo route

--- functions/index.js
+++ functions/index.js
@@ -6,12 +6,14 @@ const app = require('express')();
 const {
     getAllTodos,
     postOneTodo,
-    deleteTodo
+    deleteTodo,
+    editTodo,
 } = require('./APIs/todos')
 
 app.get('/todos', getAllTodos);
 app.post('/todo', postOneTodo);
 app.delete('/todo/:todoId', deleteTodo);
+app.put('/todo/:todoId', editTodo);
 
 
 
