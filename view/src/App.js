96459cd6d20d57a549f99196c19a16e9bc592f71
Alanis Yates <yatesalanis@gmail.com>
2020-05-26 19:12:25 -0400

    login page

--- view/src/App.js
+++ view/src/App.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import  Login  from './Components/Login';
 
 const App = () => {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <Router>
+        <div>
+          <Switch>
+            <Route exact path="/login" component={Login} />
+          </Switch>
+        </div>
+      </Router>
     </div>
   );
 }
